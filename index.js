915f212987072fce0e5e238371ca87f30e49c985
Abdul Nafeh <232994020+Abdul-Nafay-IT@users.noreply.github.com>
2026-09-23 03:14:36 +0500

    basketball scorecard finished

---
+++ index.js
@@ -0,0 +1,38 @@
+let actualScore = document.getElementById("actual_score")
+let actualScore2 = document.getElementById("actual_score1")
+
+
+let count = 0
+let count1 = 0
+
+function homeScore11() {
+    count = count + 1
+    actualScore.textContent = count
+}
+
+function homeScore12() {
+    count = count + 2
+    actualScore.textContent = count
+}
+
+function homeScore13() {
+    count = count + 3
+    actualScore.textContent = count
+}
+
+function homeScore21() {
+    count1 = count1 + 1
+    actualScore2.textContent = count1
+}
+
+function homeScore22() {
+    count1 = count1 + 2
+    actualScore2.textContent = count1
+}
+
+function homeScore23() {
+    count1 = count1 + 3
+    actualScore2.textContent = count1
+}
+
+
